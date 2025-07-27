94db2e53a84f69e0dde51217aeb3356f7332e2cf
jpsankari <jpsankari@hotmail.com>
2025-07-27 17:59:23 +0800

    Updated all files

--- module/main.tf
+++ module/main.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role_policy_attachment" "xray_policy_attachment" {
 }
 
 //Task Execution Role
-resource "aws_iam_role" "ecs_execution_role" {
+resource "aws_iam_role" "ecs_xray_execution_role" {
    name = "${var.name_prefix_base}-ecs-xray-taskexecutionrole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -35,16 +35,16 @@ resource "aws_iam_role" "ecs_execution_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_readonly" {
-  role       = aws_iam_role.ecs_execution_role.name
+  role       = aws_iam_role.ecs_xray_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "secretsmanager_rw" {
-  role       = aws_iam_role.ecs_execution_role.name
+  role       = aws_iam_role.ecs_xray_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
+  role       = aws_iam_role.ecs_xray_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
