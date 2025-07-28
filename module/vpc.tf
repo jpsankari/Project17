7319ebceca3973c8badcf3ff298c6f09a8533028
jpsankari <jpsankari@hotmail.com>
2025-07-28 21:10:34 +0800

    Updated comments

--- module/vpc.tf
+++ module/vpc.tf
@@ -7,7 +7,7 @@ tags  = {
 
 
 resource "aws_subnet" "this_subnet" {
-  vpc_id            = aws_vpc.Sankari_VPC.id
+  vpc_id            = aws_vpc.this_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-southeast-1a"
   map_public_ip_on_launch = true
