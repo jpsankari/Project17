ca1b3dda485ee5a4fa75987e59f3624882289f62
jpsankari <jpsankari@hotmail.com>
2025-07-30 22:19:17 +0800

    updated mudules

--- main.tf
+++ main.tf
@@ -64,8 +64,8 @@ module "ecs" {
       }
       assign_public_ip                   = true
       deployment_minimum_healthy_percent = 100
-      subnet_ids = "${local.prefix}_Subnet"
-      security_group_ids  = "${local.prefix}_security_group"
+      subnet_ids = "${local.prefix}_Subnet.id"
+      security_group_ids  = "${local.prefix}_security_group.id"
     }
   }
   
